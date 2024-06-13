07456260048c10cbcb90f2622f5c926ea7897ba7
Shariar Raktm <raktim21cse@gmail.com>
2024-06-14 02:27:53 +0600

    first

--- app/Http/Controllers/ProductApiController.php
+++ app/Http/Controllers/ProductApiController.php
@@ -4,34 +4,83 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ProductApiController extends Controller
 {
     public function quality()
-    {
-        $domain = '<YOUR SERVER>';
+    {    
+
+        if (!request()->number || request()->number == '' || request()->number == 'undefined' || request()->number == 'null' || request()->number == null) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Part Number is required to get quality in perameter'
+            ], 422);
+        }
+
+        
+        $domain = env('FISHBOWL_DOMAIN');
 
         $response = Http::post($domain.'/api/login', [
-            'appName' => 'Ecom Gateway',
-            'appDescription' => 'Ecom Gateway',
-            'appId' => 1995,
-            'username' => 'API',
-            'password' => 'D@t@m@t1c01'
+            'appName' => env('FISHBOWL_APP_NAME'),
+            'appDescription' => env('FISHBOWL_APP_DESCRIPTION'),
+            'appId' => env('FISHBOWL_APP_ID'),
+            'username' => env('FISHBOWL_USERNAME'),
+            'password' => env('FISHBOWL_PASSWORD')
         ], [
             'Content-Type: application/json'
         ]);
         
-        // You can then handle the response accordingly
         $status = $response->status();
+
+        if ($status != 200) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $response->json()['message']
+            ], $status);
+        }
+
         $token = $response->json()['token'];
         
+        
+
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization' => 'Bearer ' . $token
-        ])->get($domain.'/api/parts/inventory');
+        ])->get($domain.'/api/parts/inventory?pageSize=1000&number='.request()->number);
         
-        // You can then handle the response accordingly
+
         $status = $response->status();
+
+        if ($status != 200) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $response->json()['message']
+            ], $status);
+        }
+
         $data = $response->json();
+
+        $inventory = null;
+
+        foreach ($data['results'] as $product) {
+            
+            if ($product['partNumber'] == request()->number) {
+                $inventory  = $product['quantity'];
+                
+                break;
+            }
+        }
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.$token,
+        ])->post($domain.'/api/logout');
+
+        return response()->json([
+            'status' => 'Success',
+            "quantity" => $inventory
+        ]);
+        
+        
     }
 }
