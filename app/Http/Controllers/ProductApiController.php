ae6b6e73f34c79caae446e759f2bd3f384d8d122
Shariar Raktm <raktim21cse@gmail.com>
2024-07-02 00:48:40 +0600

    new api

--- app/Http/Controllers/ProductApiController.php
+++ app/Http/Controllers/ProductApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -83,4 +84,91 @@ class ProductApiController extends Controller
         
         
     }
+
+
+    public function qualityAll()
+    {    
+
+        
+        $domain = env('FISHBOWL_DOMAIN');
+
+        $response = Http::post($domain.'/api/login', [
+            'appName' => env('FISHBOWL_APP_NAME'),
+            'appDescription' => env('FISHBOWL_APP_DESCRIPTION'),
+            'appId' => env('FISHBOWL_APP_ID'),
+            'username' => env('FISHBOWL_USERNAME'),
+            'password' => env('FISHBOWL_PASSWORD')
+        ], [
+            'Content-Type: application/json'
+        ]);
+        
+        $status = $response->status();
+
+        if ($status != 200) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $response->json()['message']
+            ], $status);
+        }
+
+        $token = $response->json()['token'];
+        
+        
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ])->get($domain.'/api/parts/inventory?pageSize=100000');
+        
+
+        $status = $response->status();
+
+        if ($status != 200) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $response->json()['message']
+            ], $status);
+        }
+
+        $data = $response->json();
+
+        $allData = [];
+
+        foreach ($data['results'] as $product) {
+            
+            $databseCheck = Products::where('part_number', $product['partNumber'])->first();
+
+            if ($databseCheck) {
+
+                if ($databseCheck->quantity != $product['quantity']) {
+                    array_push($allData, $product);
+                }
+
+            }else {
+
+                Products::create([
+                    'part_number'     => $product['partNumber'],
+                    'quantity'        => $product['quantity'],
+                    'partDescription' => $product['partDescription'],
+                ]);
+
+                array_push($allData, $product);
+            }
+        }
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.$token,
+        ])->post($domain.'/api/logout');
+
+        return response()->json([
+            'status'   => 'Success',
+            "data" => $allData
+        ],200);
+        
+        
+    }
+
+
+
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::get('product/quality', [ProductApiController::class, 'quality']);
+Route::get('product/quantity/all', [ProductApiController::class, 'qualityAll']);
